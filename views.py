65d7524666c1c74f00149d7bf10a6e4e3a7d2dab
th3Moo <141202626+th3Moo@users.noreply.github.com>
2025-08-28 17:20:20 +0200

    Add files via upload

---
+++ views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import UserRegistrationSerializer, UserProfileSerializer
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
